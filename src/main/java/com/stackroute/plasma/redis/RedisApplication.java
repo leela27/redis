e23eeac0f21e163c2ffd9d7a5fb88330de7fc694
Leela27 <aleelan1@in.ibm.com>
2019-02-11 18:49:46 +0530

    redis

---
+++ src/main/java/com/stackroute/plasma/redis/RedisApplication.java
@@ -0,0 +1,30 @@
+package com.stackroute.plasma.redis;
+
+import com.stackroute.plasma.redis.domain.Item;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootApplication
+public class RedisApplication {
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory(){
+			return new JedisConnectionFactory();
+		}
+
+		@Bean
+		RedisTemplate<String, Item> redisTemplate(){
+			RedisTemplate<String,Item> redisTemplate = new RedisTemplate<String, Item>();
+			redisTemplate.setConnectionFactory(jedisConnectionFactory());
+			return redisTemplate;
+		}
+
+		public static void main(String[] args) {
+			SpringApplication.run(RedisApplication.class, args);
+		}
+	}
+
+
+
